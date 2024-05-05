80eb684e980822ba4b7dbe84e5453d6d6764feb9
Craig Swanson <craig@craigswanson.org>
2024-05-05 13:18:19 -0700

    Update server start log for clarity in production

--- app.js
+++ app.js
@@ -80,5 +80,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
